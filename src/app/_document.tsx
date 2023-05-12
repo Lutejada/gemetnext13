4d580a2bc43d68a79ff8de7b8a0b227c79de2f50
Luis Tejada <luetejava@gmail.com>
2023-05-11 20:02:27 -0500

    se agrega el dashboard

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -7,6 +7,7 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import React from "react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
