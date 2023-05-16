aea4f9fd934a6f8a84dc9e4b6ddeec410bd278bd
Andres <aneduardotejada@gmail.com>
2023-05-15 20:55:09 -0500

    handle Error y validar tipos con zod

--- src/app/api/equipo/[id]/route.ts
+++ src/app/api/equipo/[id]/route.ts
@@ -1,23 +1,19 @@
 import { NextResponse } from "next/server";
 import { equipoRespositoryImpl } from "../infraestructura/equipoRespositorioImp";
 import { obtenerEquipo } from "../aplicacion/obtenerEquipo";
-
+import { errorHandler } from "../infraestructura/error.handler";
+import { string } from "zod";
 interface Params {
   params: { id: string };
 }
 
 export async function GET(request: Request, { params }: Params) {
   try {
-    console.log(params.id); //1
+    string().uuid().parse(params.id);
     const repositorio = equipoRespositoryImpl();
     const equipo = await obtenerEquipo(repositorio, params.id);
     return NextResponse.json(equipo);
-  } catch (error:any) {
-    console.log(error.message);
-    console.log(error.cause);
-    if(error.cause === 'negocio'){
-        return NextResponse.json({ error: error.message },{status:400});
-    }
-    return  NextResponse.json({error:error.message},{status:500})
+  } catch (error: any) {
+    return errorHandler(error);
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home () {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Pagina Principal</h1>
       <p>Aqui debe estar el login</p>
     </main>
