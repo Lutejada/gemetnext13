c5a72fd39706b9819691df7861d8939946f1ae2c
Andres Tejada <62909265+andrestejada@users.noreply.github.com>
2023-06-21 20:31:01 -0500

    Sidebar (#8)
    * implementar next auth
    
    * crear usuario y validar
    
    * sidebar stilos y avatar

--- src/app/dashboard/components/Navbar.tsx
+++ src/app/dashboard/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 export default function Navbar () {
   return (
-    <nav className='col-span-1 row-span-1 w-full bg-secondary p-4'>
+    <nav className='col-span-1 row-span-1 w-full bg-secondary p-4 flex flex-row justify-between flex-wrap'>
       this is the navbar
+      <Avatar>
+        <AvatarImage src='' alt='@shadcn' />
+        <AvatarFallback className="font-semibold" >CN</AvatarFallback>
+      </Avatar>
     </nav>
   )
 }
