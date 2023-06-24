5b82c64e5b952b7fbf3a067ccbe7ded5f6abdb93
Andres Tejada <62909265+andrestejada@users.noreply.github.com>
2023-06-24 07:54:49 -0500

    Seed data (#9)
    * implementar next auth
    
    * crear usuario y validar
    
    * sidebar stilos y avatar
    
    * seed users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 async function main() {
-  
-  
+  await prisma.usuario.createMany({
+    data: [
+      { correo: "andres@correo.com", nombre: "andres", password: "12345" },
+      { correo: "luis@correo.com", nombre: "luis", password: "12345" },
+    ],
+  });
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
