8a7bf4dda578121ff829350c1a32b2f705a636b7
andrestejada <aneduardotejada@gmail.com>
2024-01-14 16:47:42 -0500

    actividades cliente id

--- src/app/api/actividad/repositorio/actividadRepositorio.ts
+++ src/app/api/actividad/repositorio/actividadRepositorio.ts
@@ -1,16 +1,28 @@
-import { prisma } from '@/src/lib/prisma';
-import { Actividad } from '../dominio';
-import { CrearActividadDto } from '../dtos/crear';
-import { ActividadRepositorio } from './index';
-export const actividadRepositorio :ActividadRepositorio={
-    crearActividad: function (dto: CrearActividadDto): Promise<Actividad> {
-        return prisma.tipo_actividad.create({
-            data: {
-                descripcion: dto.descripcion,                
-            }
-        });
-    },
-    obtenerActividades: function (): Promise<Actividad[]> {
-        return prisma.tipo_actividad.findMany()
-    }
-}
+import { prisma } from "@/src/lib/prisma";
+import { Actividad } from "../dominio";
+import { CrearActividadDto } from "../dtos/crear";
+import { ActividadRepositorio } from "./index";
+export const actividadRepositorio: ActividadRepositorio = {
+  crearActividad: async function (
+    dto: CrearActividadDto,
+    clienteId: string
+  ): Promise<Actividad> {
+    const resActividad = await prisma.tipo_actividad.create({
+      data: {
+        cliente_id: clienteId,
+        descripcion: dto.descripcion,
+      },
+    });
+    const actividad: Actividad = {
+      id: resActividad.id,
+      descripcion: resActividad.descripcion,
+      fecha_creacion: resActividad.fecha_creacion,
+      fecha_actualizacion: resActividad.fecha_actualizacion,
+      clienteId: resActividad.cliente_id,
+    };
+    return actividad;
+  },
+  obtenerActividades: function (clienteId: string): Promise<Actividad[]> {
+    return prisma.tipo_actividad.findMany({ where: { cliente_id: clienteId } });
+  },
+};
--- src/app/api/tipoPatron/repositorio/index.ts
+++ src/app/api/tipoPatron/repositorio/index.ts
@@ -1,6 +1,9 @@
 import { TipoPatron } from "../dominio";
 import { CrearTipoPatronDto } from "../dtos/crear";
 
-export interface TipoPatronRespositorio{
-    creatTipoPatron:(dto:CrearTipoPatronDto)=>Promise<TipoPatron>
+export interface TipoPatronRespositorio {
+  creatTipoPatron: (
+    dto: CrearTipoPatronDto,
+    clienteId: string
+  ) => Promise<TipoPatron>;
 }
