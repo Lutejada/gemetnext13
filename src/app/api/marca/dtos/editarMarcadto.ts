b067bcb2dad3522f3583a21510a8f296af3c0264
andrestejada <aneduardotejada@gmail.com>
2024-07-16 21:28:17 -0500

    create dto and repository of marcas

--- src/app/api/marca/dtos/editarMarcadto.ts
+++ src/app/api/marca/dtos/editarMarcadto.ts
@@ -1,13 +1,17 @@
 import { object, string } from "zod";
 
 export interface EditarMarcaDto {
+  id: string;
   identificacion?: string;
   descripcion?: string;
 }
 
 export const validarEditarMarca = (marca: EditarMarcaDto) => {
   object({
-    identificacion: string({ description: "identificacion es requerido" }).optional(),
+    id: string({description:'id es requerido'}),
+    identificacion: string({
+      description: "identificacion es requerido",
+    }).optional(),
     descripcion: string({ description: "descripcion es requerido" }).optional(),
   }).parse(marca);
 };
--- src/app/api/marca/repositorio/marcaRepositorio.ts
+++ src/app/api/marca/repositorio/marcaRepositorio.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/src/lib/prisma";
 import { Marca } from "../dominio";
 import { CrearMarcaDto } from "../dtos/crearMarca.dto";
 import { MarcaRepositorio } from "./index";
+import { EditarMarcaDto } from "../dtos/editarMarcadto";
 export const marcaRepositorio: MarcaRepositorio = {
   crearMarca: function (dto: CrearMarcaDto, clienteId: string): Promise<Marca> {
     return prisma.marca.create({
@@ -14,5 +15,17 @@ export const marcaRepositorio: MarcaRepositorio = {
   },
   obtenerMarcas: function (clienteId: string): Promise<Marca[]> {
     return prisma.marca.findMany({ where: { cliente_id: clienteId } });
+  },
+  editarMarca: async function (dto: EditarMarcaDto, clienteId: string) {
+    await prisma.marca.update({
+      where: {
+        cliente_id: clienteId,
+        id: dto.id,
+      },
+      data: {
+        descripcion: dto.descripcion,
+        identificacion: dto.identificacion,
+      },
+    });
   },
 };
