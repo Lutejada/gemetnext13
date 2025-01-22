01a98f7bf3d631bba03365d23fb3eca27d05a401
Andres Tejada <62909265+andrestejada@users.noreply.github.com>
2025-01-21 21:32:41 -0500

    Feature/forgot password (#67)
    * create user
    
    * change password success
    
    * Update error messages and improve alert handling in authentication forms
    
    * Add forgot password functionality and related DTOs; refactor change password DTO for consistency
    
    * Fix base domain URL handling for local and production environments
    
    * Update login button text and add debug log for base domain generation

--- src/lib/helpers/getBaseDomain.ts
+++ src/lib/helpers/getBaseDomain.ts
@@ -1,6 +1,7 @@
 import { Cliente } from "@/app/api/cliente/dominio/entity";
 
 export const getBaseDomain = (cliente: Cliente): string => {
+  console.log("process.env.DOMAIN", process.env.DOMAIN);
   let baseDomain = `https://${cliente.nombre}.${process.env.DOMAIN}`;
   if (process.env.APP_ENV === "local") {
     baseDomain = `http://${cliente.nombre}.${process.env.DOMAIN}`;
